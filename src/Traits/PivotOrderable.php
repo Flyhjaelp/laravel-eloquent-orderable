0e0f73cdfd2f19f7336971cd792b4a082f931f91
Philip Brechmann <pb@brechmann.dk>
2019-04-26 14:57:23 +0200

    Apply fixes from StyleCI (#2)

--- src/Traits/PivotOrderable.php
+++ src/Traits/PivotOrderable.php
@@ -1,130 +1,131 @@
 <?php
 
-
 namespace Flyhjaelp\LaravelEloquentOrderable\Traits;
 
-
 use Exception;
-use Flyhjaelp\LaravelEloquentOrderable\Events\OrderableModelCreating;
+use Illuminate\Support\Collection;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 use Flyhjaelp\LaravelEloquentOrderable\Events\OrderableModelDeleted;
 use Flyhjaelp\LaravelEloquentOrderable\Events\OrderableModelUpdated;
+use Flyhjaelp\LaravelEloquentOrderable\Events\OrderableModelCreating;
 use Flyhjaelp\LaravelEloquentOrderable\Events\OrderableModelUpdating;
 use Flyhjaelp\LaravelEloquentOrderable\Interfaces\OrderableInterface;
-use Illuminate\Database\Eloquent\Relations\Pivot;
-use Illuminate\Support\Collection;
 
-trait PivotOrderable {
+trait PivotOrderable
+{
+    use OrderableWithinGroup;
 
-   use OrderableWithinGroup;
+    /**
+     * @var Collection;
+     */
+    protected static $originalOrderValues;
 
-   /**
-    * @var Collection;
-    */
-   protected static $originalOrderValues;
+    public static function bootOrderableWithinGroup(): void
+    {
+        static::setOriginalOrderValues();
 
-   public static function bootOrderableWithinGroup(): void
-   {
+        static::creating(function (OrderableInterface $orderableModel) {
+            static::fireEventIfNotUpdatingOrderAlready(OrderableModelCreating::class, $orderableModel);
+        });
 
-      static::setOriginalOrderValues();
+        static::updating(function (OrderableInterface $orderableModel) {
+            $orderableModel->fillWithDataFromDb();
+            static::fireEventIfNotUpdatingOrderAlready(OrderableModelUpdating::class, $orderableModel);
+        });
 
-      static::creating(function (OrderableInterface $orderableModel) {
-         static::fireEventIfNotUpdatingOrderAlready(OrderableModelCreating::class, $orderableModel);
-      });
+        static::updated(function (OrderableInterface $orderableModel) {
+            $orderableModel->fillWithDataFromDb();
+            static::fireEventIfNotUpdatingOrderAlready(OrderableModelUpdated::class, $orderableModel);
+        });
 
-      static::updating(function (OrderableInterface $orderableModel) {
-         $orderableModel->fillWithDataFromDb();
-         static::fireEventIfNotUpdatingOrderAlready(OrderableModelUpdating::class, $orderableModel);
-      });
+        static::deleting(function (OrderableInterface $orderableModel) {
+            $orderableModel->fillWithDataFromDb();
+        });
 
-      static::updated(function (OrderableInterface $orderableModel) {
-         $orderableModel->fillWithDataFromDb();
-         static::fireEventIfNotUpdatingOrderAlready(OrderableModelUpdated::class, $orderableModel);
-      });
+        static::deleted(function (OrderableInterface $orderableModel) {
+            static::fireEventIfNotUpdatingOrderAlready(OrderableModelDeleted::class, $orderableModel);
+        });
 
-      static::deleting(function (OrderableInterface $orderableModel) {
-         $orderableModel->fillWithDataFromDb();
-      });
+        static::addGlobalOrderingScope();
+    }
 
-      static::deleted(function (OrderableInterface $orderableModel) {
-         static::fireEventIfNotUpdatingOrderAlready(OrderableModelDeleted::class, $orderableModel);
-      });
+    protected static function setOriginalOrderValues()
+    {
+        if (static::$originalOrderValues === null) {
+            static::$originalOrderValues = collect();
+        }
+    }
 
-      static::addGlobalOrderingScope();
-   }
+    public function getOriginalOrderValue(): ?int
+    {
+        if (static::$originalOrderValues->has($this->getOriginalOrderValueKey())) {
+            return static::$originalOrderValues[$this->getOriginalOrderValueKey()];
+        }
 
-   protected static function setOriginalOrderValues() {
-      if( static::$originalOrderValues === null){
-         static::$originalOrderValues = collect();
-      }
-   }
+        return null;
+    }
 
-   public function getOriginalOrderValue(): ?int {
+    protected function getOriginalOrderValueKey(): string
+    {
+        $keyName = $this->getKeyName();
+        if ($this->canBeFoundViaPrimaryKey()) {
+            return 'I'.$this->$keyName;
+        }
 
-      if(static::$originalOrderValues->has($this->getOriginalOrderValueKey())){
-         return static::$originalOrderValues[$this->getOriginalOrderValueKey()];
-      }
+        throw new Exception('Can\'t get original order value key as both the "Foreign key" and "Primary key" aren\'t set');
+    }
 
-      return null;
+    protected function canBeFoundViaForeignKeys(): bool
+    {
+        $foreignKey = $this->getForeignKey();
+        $relatedKey = $this->getRelatedKey();
 
-   }
+        return $foreignKey !== null && $relatedKey !== null;
+    }
 
-   protected function getOriginalOrderValueKey(): string{
+    protected function canBeFoundViaPrimaryKey(): bool
+    {
+        $keyName = $this->getKeyName();
 
-      $keyName = $this->getKeyName();
-      if($this->canBeFoundViaPrimaryKey()){
-         return 'I'.$this->$keyName;
-      }
+        return $this->$keyName !== null;
+    }
 
-      throw new Exception('Can\'t get original order value key as both the "Foreign key" and "Primary key" aren\'t set');
+    protected function fillWithDataFromDb(): void
+    {
+        $newData = $this->toArray();
 
-   }
-
-   protected function canBeFoundViaForeignKeys(): bool {
-      $foreignKey = $this->getForeignKey();
-      $relatedKey = $this->getRelatedKey();
-
-      return $foreignKey !== null && $relatedKey !== null;
-   }
-
-   protected function canBeFoundViaPrimaryKey(): bool {
-      $keyName = $this->getKeyName();
-      return $this->$keyName !== null;
-   }
-
-   protected function fillWithDataFromDb(): void{
-         $newData = $this->toArray();
-
-         if($this->canBeFoundViaForeignKeys()){
+        if ($this->canBeFoundViaForeignKeys()) {
             $oldData = $this->findModelViaForeignKeys()->toArray();
-         }else if($this->canBeFoundViaPrimaryKey()){
+        } elseif ($this->canBeFoundViaPrimaryKey()) {
             $oldData = $this->findModelViaPrimaryKey()->toArray();
-         }else{
+        } else {
             throw new Exception('Can\'t fill model as both the "Foreign key" and "Primary key" aren\'t set');
-         }
+        }
 
-         $this->fill($oldData);
-         if(!static::$originalOrderValues->has($this->getOriginalOrderValueKey())){
+        $this->fill($oldData);
+        if (! static::$originalOrderValues->has($this->getOriginalOrderValueKey())) {
             static::$originalOrderValues[$this->getOriginalOrderValueKey()] = $oldData[$this->getOrderableColumn()];
-         }
-         $this->fill($newData);
-   }
+        }
+        $this->fill($newData);
+    }
 
-   protected function findModelViaForeignKeys(): Pivot {
-      $relatedKey = $this->getRelatedKey();
-      $foreignKey = $this->getForeignKey();
-      return $this
+    protected function findModelViaForeignKeys(): Pivot
+    {
+        $relatedKey = $this->getRelatedKey();
+        $foreignKey = $this->getForeignKey();
+
+        return $this
          ->where($relatedKey, $this->$relatedKey)
          ->where($foreignKey, $this->$foreignKey)
          ->firstOrFail();
-   }
+    }
 
-   protected function findModelViaPrimaryKey(): Pivot {
-      $primaryKey = $this->getKeyName();
-      return $this
+    protected function findModelViaPrimaryKey(): Pivot
+    {
+        $primaryKey = $this->getKeyName();
+
+        return $this
          ->where($primaryKey, $this->$primaryKey)
          ->firstOrFail();
-   }
-
-
-
+    }
 }
